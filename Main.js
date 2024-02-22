230ad3fef1f30bddf961bbcf1225652cfa9fd5af
yoramvdh <yoram.vandenhouwe@student.howest.be>
2024-02-22 18:29:54 +0100

    Main-expiredProduct(Inventorymanagement system)- ()-22/02/2024: Change name of function vervallen product, refactoring, translating to english
    Main-expiredProduct(Inventorymanagement system)- ()-22/02/2024: Change name of function vervallen product, refactoring, translating to english

--- Main.js
+++ Main.js
@@ -1,58 +1,71 @@
 /* eslint-disable require-jsdoc */
 /* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
-// project: Voorraadbeheersysteem
-// functie: Een semi-automatisch voorraadbeheersysteem.
-// Deze applicatie is ontwikkeld voor het anatomo-pathologisch labo van AZ Zeno.
-// Naam: Yoram Vandenhouwe
-// Datum  start aanmaak: 13/02/2024
-// Datum einde aanmaak:
-// Versie: 0.1
+// project: Inventorymanagement system
+// functie: Een semi-automatic Inventorymanagement system.
+// This application is develloped for the pathology labo fo AZ Zeno.
+// Name: Yoram Vandenhouwe
+// Start of project: 13/02/2024
+// Version: 0.1
 
+/* Declaration*/
 
-// vult de  huidige datum in de gegeven cell
-function AddDate(sheet, rij, kolom) {
-  var time = new Date();
+let row = 3; // begin van de tabel
+// Get SpreadsheetUrl
+const sheetUrl = SpreadsheetApp.getActive().getUrl();
+// Get all the sheets
+const sheets = SpreadsheetApp.getActive().getSheets();
+const voorraadbeheer = sheets[0];
+const vervallenReagentia = sheets[1];
+
+// Create array to store all the links
+const links = [];
+// For each sheet in sheets add an array element to our array with the
+// string of the URL for that sheet
+sheets.forEach((sheet)=>links.push(sheetUrl+'#gid='+sheet.getSheetId()));
+const voorraadbeheerlink = links[0];
+const vervallenReagentialink = links[2];
+
+const range = voorraadbeheer.getRange('A3:01100');
+
+// Function to add the current date in a given cell.
+function addDate(sheet, row, column) {
+  let time = new Date();
   time = Utilities.formatDate(time, 'GMT+02:00', 'dd/MM/yy');
-  sheet.getRange(rij, kolom).setValue(time);
+  sheet.getRange(row, column).setValue(time);
 }
 
-// spoort via een trigger vervallen producten op en verplaatst een deze in het tabblad vervallen reagentia, stuurt ook mail naar alle MLT
-function VervallenProducten() {
-  // declaratie
-  var voorraadbeheer = SpreadsheetApp.getActive().getSheetByName('voorraadbeheer');
-  var vervallenReagentia = SpreadsheetApp.getActive().getSheetByName('vervallen reagentia');
-  var row = 3; // begin van de tabel
-  var vervallen = 0; // vervaldatum
-  var huidigeCell = voorraadbeheer.getRange(row, 1).getValue();
+/* Activates using a trigger in the Google App Script aplication. If the product is expired moves all data of this product to a separate sheet to store the data.
+Then send a mail to all mail adresses in the config sheet. */
+function expiredProduct() {
+  // declaration
+  const expired = 0; // Experationdate
+  let currentcell = voorraadbeheer.getRange(row, 1).getValue();
 
-  // zolang de rij niet leeg is overloopt de functie de tabel en vergelijk de datum met de vervaldatum
+  // While the row is not empty, check each row to see if the product is expired.
+  while ( currentcell != '') {
+    const experationdate = voorraadbeheer.getRange(row, 9).getValue();
+    const emptycell =voorraadbeheer.getRange(row, 11).getValue();
+    // If expired.
+    if (experationdate == expired && emptycell == '' ) {
+      addDate(voorraadbeheer, row, 12); // Add the date in colum 12.
 
-  while ( huidigeCell != '') {
-    var houdbaarheidsDatum = voorraadbeheer.getRange(row, 9).getValue();
-    var legecell =voorraadbeheer.getRange(rowv, 11).getValue();
-    // als de houdbaarheid vervallen is
-    if (houdbaarheidsDatum == vervallen && legecell == '' ) {
-      AddDate(voorraadbeheer, row, 12); // plaatsen we in kolom 12 de datum
+      // Cuts the row and places the data in a new line in sheet 'vervallen reagentia'.
+      const expiredProduct = voorraadbeheer.getRange(row, 1, 1, 15);
+      const destRange = vervallenReagentia.getRange(vervallenReagentia.getLastRow()+1, 1);
+      expiredProduct.copyTo(destRange, {contentsOnly: false});
+      expiredProduct.clear();
 
-      // kopier de lijn en plaats deze is de sheet vervallen reagentia
-      var vervallenLijn = voorraadbeheer.getRange(row, 1, 1, 15);
-      var destRange = vervallenReagentia.getRange(vervallenReagentia.getLastRow()+1, 1);
-      vervallenLijn.copyTo(destRange, {contentsOnly: false});
-      vervallenLijn.clear();
-
-      // stuurt een email als er een product vervallen is
+      // Send a mail if a product is expired.
       MailApp.sendEmail({to: 'yoram.vandenhouwe@azzenopathologie.net',
-        subject: 'automatische mail-Vervallen product',
-        htmlBody: 'Zie tablad Vervallen Producten   https://docs.google.com/spreadsheets/d/1e5JcF0UiyphQIlexmdSrpDlXzzvxRfpcHvutMr6iB14/edit#gid=1452909895 ',
+        subject: 'automatic mail-Expired product',
+        htmlBody: 'The product, '+ currentcell + ', has expired and was placed in the tab vervallen reagentia on the last row.For more information use the link:' + vervallenReagentialink,
       });
     }
-    row = row +1; // ga naar de volgende niet lege rij
-    huidigeCell = voorraadbeheer.getRange(row, 1).getValue();
+    row = row +1; // While the row is not empty, check each row to see if the product is expired.
+    currentcell = voorraadbeheer.getRange(row, 1).getValue();
   }
-
-  var range = voorraadbeheer.getRange('A3:01100');
-  range.sort(7); // op welke colom er gesorteerd wordt
+  range.sort(7); // Sort complete range based on column.
 }
 
 
