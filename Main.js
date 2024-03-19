7bea52ef4641399edf8671628e76290e3825f24b
yoramvdh <yoram.vandenhouwe@student.howest.be>
2024-03-19 16:08:55 +0100

    Main-onEdit()/onOpen()/totalSupply()- (voorraadbeheer)-19/03/2024: Made on edit functions to track if a product was used for the first time or was used for the last time. Made function totalSupply which counts the number of items a selected product has in the current inventory. Made a menu in the Ui.
    Main-onEdit()/onOpen()/totalSupply()- (voorraadbeheer)-19/03/2024: Made on edit functions to track if a product was used for the first time or was used for the last time. Made function totalSupply which counts the number of items a selected product has in the current inventory. Made a menu in the Ui.

--- Main.js
+++ Main.js
@@ -27,13 +27,23 @@ sheets.forEach((sheet)=>links.push(sheetUrl+'#gid='+sheet.getSheetId()));
 const voorraadbeheerlink = links[0];
 const vervallenReagentialink = links[3];
 
-const range = voorraadbeheer.getRange('A3:01100');
+const maxrange = voorraadbeheer.getRange('A3:01100');
 
 // Function to add the current date in a given cell.
 function addDate(sheet, row, column) {
   let time = new Date();
   time = Utilities.formatDate(time, 'GMT+02:00', 'dd/MM/yy');
   sheet.getRange(row, column).setValue(time);
+}
+
+// Add a custom menu to the active spreadsheet, including a separator and a sub-menu.
+function onOpen(e) {
+  SpreadsheetApp.getUi()
+      .createMenu('Specialised Functions')
+      .addItem('Total Supply', 'totalSupply')
+      .addSeparator()
+      .addItem('My menu item', 'myFunction')
+      .addToUi();
 }
 
 /* Activates using a trigger in the Google App Script aplication. If the product is expired moves all data of this product to a separate sheet to store the data.
@@ -66,7 +76,7 @@ function expiredProduct() {
     row = row +1; // While the row is not empty, check each row to see if the product is expired.
     currentcell = voorraadbeheer.getRange(row, 1).getValue();
   }
-  range.sort(7); // Sort complete range based on column.
+  maxrange.sort(7); // Sort complete range based on column.
 }
 
 // This function uses a trigger to find all product who are almoust expired and send a mail to specifiek users
@@ -94,7 +104,6 @@ function almoustExpiredProducts() {
 // This function uses a trigger to find all product used up and moves the data to a seperate sheet.
 
 function usedUp() {
-
   let currentcell = voorraadbeheer.getRange(row, 1).getValue();
 
   // As long as the currentcell is not empty the function goes over the table and will compare each time the expirationdate with the number of days till it expires.
@@ -112,9 +121,71 @@ function usedUp() {
     currentcell = voorraadbeheer.getRange(row, 1).getValue();
   }
 
-
-  const range = voorraadbeheer.getRange('A3:01100');
-  range.sort(7); // sort the whole range on column 7
+  maxrange.sort(7); // sort the whole range on column 7
 }
 
 
+function totalSupply() {
+  const selObj = SpreadsheetApp.getActiveSheet().getSelection();
+  const selectedText = selObj.getActiveRange().getValue();
+  voorraadbeheer.activate();
+  let currentcell = voorraadbeheer.getRange(row, 1).getValue();
+  if (selectedText != '') {
+    let totalitems = 0;
+    let currentitem = 0;
+    Logger.log(totalitems);
+    Logger.log(currentitem);
+    while ( currentcell != '') {
+      // eslint-disable-next-line prefer-const
+      let itemId =voorraadbeheer.getRange(row, 1).getValue();
+      if ( itemId == selectedText ) {
+        Logger.log(itemId);
+        // eslint-disable-next-line prefer-const
+        let maxitem = voorraadbeheer.getRange(row, 4).getValue();
+        // eslint-disable-next-line prefer-const
+        let useditem = voorraadbeheer.getRange(row, 6).getValue();
+        currentitem = maxitem- useditem;
+        Logger.log(currentitem);
+        Logger.log(totalitems);
+        totalitems = totalitems + currentitem;
+        Logger.log(totalitems);
+      }
+      row = row +1; // ga naar de volgende niet lege rij
+      currentcell = voorraadbeheer.getRange(row, 1).getValue();
+    }
+    SpreadsheetApp.getUi().alert('The total number of item '+selectedText+' is: '+ totalitems);
+  } else {
+    SpreadsheetApp.getUi().alert('Select the item you want to check the inventory for out of the list of in the minimum voorraad tab. Then run the function again.');
+  }
+}
+
+
+function onEdit(e) {
+  // declaration
+  // the follow code is execute each time a change was made in google sheets
+  // eslint-disable-next-line prefer-const
+  let activerange = e.range;
+  // eslint-disable-next-line prefer-const
+  let activerow = activerange.getRow();
+  // eslint-disable-next-line prefer-const
+
+
+  // Check on edit if the number of items in that row is equal to the max number of items in that row. If this maches then a the current date is placed in column 11 ( "opgebruikt")
+  if (SpreadsheetApp.getActiveSheet().getName() =='voorraadbeheer') {
+    const maxitems = voorraadbeheer.getRange(activerow, 4).getValue();
+    const currentstock = voorraadbeheer.getRange(activerow, 6).getValue();
+    const emptycell =voorraadbeheer.getRange(activerow, 11).getValue();
+    if (maxitems == currentstock && maxitems != 0 && emptycell == '') {
+      addDate(voorraadbeheer, row, 11);
+    }
+  }
+  // Checks on the active line when a product is used for the first time, when it is: add date in column 10 "ingebruikname"
+  if (SpreadsheetApp.getActiveSheet().getName() =='voorraadbeheer') {
+    const huidigAantal = voorraadbeheer.getRange(row, 6).getValue();
+    const emptycell =voorraadbeheer.getRange(row, 10).getValue();
+
+    if (huidigAantal != 0 && emptycell == '' ) {
+      addDate(voorraadbeheer, row, 10);
+    }
+  }
+}
