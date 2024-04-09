88b995b7e14d09e7c104660169d0807ae613c2d4
yoramvdh <yoram.vandenhouwe@student.howest.be>
2024-04-09 19:36:23 +0200

    Main- itemPresentInList()/ minimumSupply()- (voorraadbeheer)-09/04/2024: MinimumSupply function checks all the items in the supply and calculates the total supply and compares these with the minimum supply necessary. If below a treshold adds them to the list if not already present.
    MinimumSupply loops over a list in the tab minimum voorraad, for each item in the list it loops over the list in the tab voorraadbeheer and calculates the total supplies present for the item in minimum voorraad.
    The calculation is done by subtracting the used items from the total items. Next the item is compared to a list of minimum item quantities, if the item is lower than the minimum than the te bestellen page and the besteld page are checked to see if the item is present in the first 30 lines. If the item is present then update the current supplies, if not present search the first empty line and add the item name, the active supply and the current date.
    itemPresentInList takes the name of the tab and the name of and item and the column number. It will then check if it can find the name in the specified column.
    
    returns an array: alreadyInList is true when the item is present, false if not. rowWithItem: is 0 when not found, otherwise it will be the row number where the name was found.

--- Main.js
+++ Main.js
@@ -1,4 +1,4 @@
-/* eslint-disable camelcase */
+
 /* eslint-disable require-jsdoc */
 /* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
@@ -17,8 +17,14 @@ const sheetUrl = SpreadsheetApp.getActive().getUrl();
 // Get all the sheets
 const sheets = SpreadsheetApp.getActive().getSheets();
 const voorraadbeheer = sheets[0];
-const vervallenReagentia = sheets[3];
-const opgebruikteReagentia = sheets[2];
+const minimumVoorraad = sheets[1];
+const teBestellen = sheets[2];
+const besteld = sheets[3];
+const opgebruikteReagentia = sheets[4];
+const vervallenReagentia = sheets[5];
+const statistieken = sheets[6];
+const configuration = sheets[7];
+
 
 // Create array to store all the links
 const links = [];
@@ -26,7 +32,7 @@ const links = [];
 // string of the URL for that sheet
 sheets.forEach((sheet)=>links.push(sheetUrl+'#gid='+sheet.getSheetId()));
 const voorraadbeheerlink = links[0];
-const vervallenReagentialink = links[3];
+const vervallenReagentialink = links[5];
 
 const maxrange = voorraadbeheer.getRange('A3:01100');
 
@@ -41,9 +47,9 @@ function addDate(sheet, row, column) {
 function onOpen(e) {
   SpreadsheetApp.getUi()
       .createMenu('Specialised Functions')
-      .addItem('Total Supply', 'totalSupply')
+      .addItem('Total Supply of one item', 'totalSupply')
       .addSeparator()
-      .addItem('My menu item', 'myFunction')
+      .addItem('Calculate supplies', 'minimumSupply')
       .addToUi();
 }
 
@@ -67,11 +73,12 @@ function expiredProduct() {
       const destRange = vervallenReagentia.getRange(vervallenReagentia.getLastRow()+1, 1);
       expiredProduct.copyTo(destRange, {contentsOnly: false});
       expiredProduct.clear();
-
+      // Get list of emails in the config tab.
+      const emailList= getRowOfData(configuration, 8);
       // Send a mail if a product is expired.
-      MailApp.sendEmail({to: 'yoram.vandenhouwe@azzenopathologie.net',
+      MailApp.sendEmail({to: emailList,
         subject: 'automatic mail-Expired product',
-        htmlBody: 'The product, '+ currentcell + ', has expired and was placed in the tab vervallen reagentia on the last row.For more information use the link:' + vervallenReagentialink,
+        htmlBody: 'The product, '+ currentcell + ', has expired and was placed in the tab vervallen reagentia on the last row. For more information use the link:' + vervallenReagentialink,
       });
     }
     row = row +1; // While the row is not empty, check each row to see if the product is expired.
@@ -91,10 +98,12 @@ function almoustExpiredProducts() {
     const expiredate = voorraadbeheer.getRange(row, 9).getValue();
     const alreadyused =voorraadbeheer.getRange(row, 11).getValue();
     if (expiredate == almoustexpired && alreadyused === '' ) { // Checks if the product is almost expired.
-      // Sends a mail to the user.
-      MailApp.sendEmail({to: 'yoram.vandenhouwe@azzenopathologie.net',
+      // Get list of emails in the config tab.
+      const emailList= getRowOfData(configuration, 9);
+      // Sends a mail to the users.
+      MailApp.sendEmail({to: emailList,
         subject: 'automatische mail- Bijna Vervallen product',
-        htmlBody: 'Het product '+currentcell+' op rij '+row+' zal over 14 dagen vervallen.'+ voorraadbeheerlink,
+        htmlBody: 'Het product '+currentcell+' op rij '+row+' zal over 14 dagen vervallen: '+ voorraadbeheerlink,
       });
     }
     row = row +1; // Go the the next empty row.
@@ -127,10 +136,9 @@ function usedUp() {
 // Calculates the total supply of a selected item.
 function totalSupply() {
   // start of itemarray
-  const itemarr = [['Item name', 'Expiration date ', 'Number of items']];
+  const itemarr = [['Item name', 'Lotnumber', 'Expiration date ', 'Number of items']];
   // Get the value of the cell selected in the spreadsheet.
-  const selObj = SpreadsheetApp.getActiveSheet().getSelection();
-  const selectedText = selObj.getActiveRange().getValue();
+  const selectedText = configuration.getRange(4, 2).getValue();
   // Selects the first tab.
   voorraadbeheer.activate();
   // Get the name of the first item in the list.
@@ -155,10 +163,12 @@ function totalSupply() {
         const arr = []; // Generates a new array and adds the name of the item
         arr.push(selectedText);
         // Takes the expirationdate out from the selected line. Transforms it to string then slices the string to keep relevant information.
-        const expirationdate_Obj = voorraadbeheer.getRange(row, 8).getValue();
-        const expirationdate_String= expirationdate_Obj.toString();
-        const expirationdate = expirationdate_String.slice(0, 16);
+        const expirationdateObj = voorraadbeheer.getRange(row, 8).getValue();
+        const expirationdateString= expirationdateObj.toString();
+        const expirationdate = expirationdateString.slice(0, 16);
+        const lotNumber = voorraadbeheer.getRange(row, 3).getValue();
         // Adds items to array.
+        arr.push(lotNumber);
         arr.push(expirationdate);
         arr.push(currentitem);
         // Adds the new array to the existing table.
@@ -244,7 +254,7 @@ function createDoc(itemname, totalitems, itemarr) {
 
 
 // Searches a file in the drive, check to see if the given foldername exist in the drive, if not the script creates it, then moves the file in the folder.
-function moveFile(name_Of_Destination, id_Of_File) {
+function moveFile(nameOfDestination, idOfFile) {
   let folderpresent = false; // initiates parameter to check if folder is available
   let folderid = ''; // initiates the ID string for the folder
 
@@ -255,7 +265,7 @@ function moveFile(name_Of_Destination, id_Of_File) {
     const folder = folders.next();
     const foldername = folder.getName();
 
-    if (foldername === name_Of_Destination) {
+    if (foldername === nameOfDestination) {
     // if the folder is present,change folderpresent too true and get the id
       folderpresent = true;
       folderid = folder.getId();
@@ -263,12 +273,38 @@ function moveFile(name_Of_Destination, id_Of_File) {
   }
   // if the folder does not exist create the folder and get the ID
   if (folderpresent === false) {
-    folderid = DriveApp.createFolder(name_Of_Destination).getId();
+    folderid = DriveApp.createFolder(nameOfDestination).getId();
   }
   // get the folder and move file to folder.
   const correctfolder = DriveApp.getFolderById(folderid);
-  DriveApp.getFileById(id_Of_File).moveTo(correctfolder);
+  DriveApp.getFileById(idOfFile).moveTo(correctfolder);
+  const fileUrl=DriveApp.getFileById(idOfFile).getUrl();
+
+  SpreadsheetApp.getUi().alert('The file is available the '+nameOfDestination+' folder. ', 'This is the link to the file: \n '+fileUrl, SpreadsheetApp.getUi().ButtonSet.OK);
 }
+
+// This function gets all the data from 1 row from a selected sheet en concatinates its in a comma sepperated string.
+function getRowOfData(sheet, rowNumber) {
+  let columnNumber = 2; // starting with column 2
+  // Uses the coördinates to get the chosen value
+  let currentcell = sheet.getRange(rowNumber, columnNumber).getValue();
+  // Loops over the row untill a cell is empty en concatinates each new item with the previous items.
+  let emailList= '';
+  while ( currentcell != '') {
+    // eslint-disable-next-line prefer-const
+    let emailItem = sheet.getRange(rowNumber, columnNumber).getValue();
+
+
+    emailList= emailList +','+ emailItem;
+
+    columnNumber = columnNumber +1; // go to the next not empty row
+    currentcell = sheet.getRange(rowNumber, columnNumber).getValue();
+  }
+  emailList=emailList.slice(1); // Remove the comma at the start of the string.
+
+  return emailList; // return the string.
+}
+
 
 function onEdit(e) {
   // declaration
@@ -299,5 +335,90 @@ function onEdit(e) {
     }
   }
 }
+// This function loops over a list in the tab minimum voorraad, for each item in the list it loops over the list in the tab voorraadbeheer and calculated the total supplies present for the item in minimum voorraad.
+// The calculation is done by suptracting the used items from the total items. Next the item is compared to a list of minimum item kwantities, if the item is lower then the minimum than the te bestellen page and the besteld page are checked to see if the item is present in the first 30 lines. If the item is present then update the current supplies, if not present search the first empty line and add the itemname, the active supply and the current date.
+function minimumSupply() {
+  let rowMin= 2; // Begin row of the tab minimum voorraad.
+
+  let itemId = minimumVoorraad.getRange(rowMin, 1).getValue(); // The name of the item on specified row on the minimum voorraad tab.
+
+  // As long as the currentcell is not empty the function goes over the table in minimum voorraad.
+  while ( itemId != '') {
+    let row = 2; // Begin row of the tab voorraad.
+    // Loop over all items in the tab voorraadbeheer.
+    let selectedText = voorraadbeheer.getRange(row, 1).getValue(); // The name of the item on specified row on the voorraadbeheer tab.
+
+    let totalitems = 0; // Set the total counted items to 0.
+    while (selectedText!= '') { // Loop over all items in the tab voorraadbeheer untill there is an empty row.
+      if (itemId == selectedText) { // If the names of the items in minimum voorraad and in the vooraadbeheer tab match.
+        // Calculate the current number of items by taking the maxium number of items and subscracting the used items.
+        const maxitem = voorraadbeheer.getRange(row, 4).getValue();
+        // eslint-disable-next-line prefer-const
+        let useditem = voorraadbeheer.getRange(row, 6).getValue();
+        currentitem = maxitem- useditem;
+        // added the number of used items in this instance to the number of total items.
+        totalitems = totalitems + currentitem;
+      }
+      row = row +1; // go to the next not empty row in the voorraadbeheer tab.
+
+      selectedText = voorraadbeheer.getRange(row, 1).getValue(); // Get the next name in the table.
+    }
+    // Sets the calculated number of supplys in the tabel next to the correct item.
+    minimumVoorraad.getRange(rowMin, 2).setValue(totalitems);
+
+    // eslint-disable-next-line prefer-const
+    let minimumSupply = minimumVoorraad.getRange(rowMin, 3).getValue(); // Get the minimum supply for a product from the tab minimum voorraad.
+
+    if (totalitems < minimumSupply ) { // If the current value is lower then the minimul supply.
+      const resultTeBestellen = itemPresentInList(teBestellen, itemId, 2); // Look if the item is already present in the tab te bestellen and notate the row if so.
+      const inListTeBestellen=resultTeBestellen.alreadyInList; // If values was in list then this is true.
+      const itemRowTeBestellen=resultTeBestellen.rowWithItem;
+
+      const resultBesteld = itemPresentInList(besteld, itemId, 2); // Look if the item is already present in the tab besteld and notate the row if so.
+      const inListBesteld=resultBesteld.alreadyInList; // If values was in list then this is true.
+      const itemRowBesteld=resultBesteld.rowWithItem;
+
+      if (inListTeBestellen== true) { // If the item was in the list te bestellen then update the number of items to the current supply.
+        teBestellen.getRange(itemRowTeBestellen, 3).setValue(totalitems);
+      }
+      if (inListBesteld== true) { // If the item was in the list besteld then update the number of items to the current supply.
+        besteld.getRange(itemRowBesteld, 3).setValue(totalitems);
+      }
+      if (inListTeBestellen == false && inListBesteld== false ) { // If it is not present in both lists above then add it to the list of te bestellen items.
+        let rowTeBes=2;
+        let emptycell = teBestellen.getRange(rowTeBes, 2).getValue();
+
+        while (emptycell!='') { // Find the next empty line of te bestellen.
+          rowTeBes = rowTeBes +1;
+          emptycell = teBestellen.getRange(rowTeBes, 2).getValue();
+        }
+        addDate(teBestellen, rowTeBes, 1); // Add the date.
+        teBestellen.getRange(rowTeBes, 2).setValue(itemId); // Add the name of the item.
+        teBestellen.getRange(rowTeBes, 3).setValue(totalitems); // Add the current number of items.
+      }
+    }
 
 
+    rowMin = rowMin +1; // Go to the next not empty row
+
+    itemId = minimumVoorraad.getRange(rowMin, 1).getValue(); // Get the name of the item of the row.
+  }
+}
+
+// This function takes the name of the tab and the name of and item and the column number. It will then check if it can find the name in the specified column.
+// returns an array: alreadyInList is true when the item is present, false if not. rowWithItem: is 0 when not found, otherwise it will be the row number where the name was found.
+function itemPresentInList(sheet, itemId, columnNumber) {
+  let alreadyInList = false; // set the default as false
+  let rowWithItem =0; //
+  for (let i = 2; i < 30; i++) { // Loop over the first 30 items in the given tab, in the given column.
+    const itemInList = sheet.getRange(i, columnNumber).getValue(); // Get the name of the item in the list.
+    if (itemInList == itemId) { // Compare the name of the item in the list to the given name in the function.
+      alreadyInList = true; // If the same name is found set variables to true and note the number of the line.
+      rowWithItem = i;
+    }
+  }
+  return { // Return array with bolean to see if item is present and integer of the row where it was found ( 0 if not found)
+    alreadyInList: alreadyInList,
+    rowWithItem: rowWithItem,
+  };
+}
